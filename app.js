643d1cff52d60d084a401df1edc0c986bc8b2e4d
Tom Dunn <tdunn@artofcontext.com>
2019-02-21 09:20:18 -0500

    Refactor cell code to static functions instead of all in app.js.   40x40 : 60.1 fps 60x60 : 60.1 fps 70x70 : 60.1 fps 80x80 : 60.1 fps 150x150 : 60.1 400x400 : 12.5 fps

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const cellCountY = 400;
 const cellWidth = stageWidth / cellCountX;
 const cellHeight = stageHeight / cellCountY;
 
-// create a renderer instance
-//var renderer = new PIXI.CanvasRenderer(800, 600);//PIXI.autoDetectRenderer(800, 600);
+Cell.size(cellWidth, cellHeight);
+
 var renderer = PIXI.autoDetectRenderer(stageWidth, stageHeight, {
     backgroundColor: 0x00ff00
 });
@@ -16,69 +16,17 @@ var renderer = PIXI.autoDetectRenderer(stageWidth, stageHeight, {
 console.log(`Canvas: ${PIXI.RENDERER_TYPE.CANVAS} WebGL ${PIXI.RENDERER_TYPE.WEBGL}`);
 console.log(`This renderer: ${renderer.type}`);
 
-// set the canvas width and height to fill the screen
-//renderer.view.style.width = window.innerWidth + "px";
-//renderer.view.style.height = window.innerHeight + "px";
 renderer.view.style.display = "block";
-
-// add render view to DOM
 document.body.appendChild(renderer.view);
 
-const board = new Board(renderer, stage);
-Cell.size(cellWidth, cellHeight);
-
-let g = new PIXI.Graphics();
-g.beginFill(0x000000);
-g.drawRect(0, 0, Cell.width, Cell.height);
-const onTexture = PIXI.RenderTexture.create(g.width, g.height);
-renderer.render(g, onTexture);
-
-g = new PIXI.Graphics();
-g.beginFill(0xFFFFFF);
-g.drawRect(0, 0, Cell.width, Cell.height);
-const offTexture = PIXI.RenderTexture.create(g.width, g.height);
-renderer.render(g, offTexture);
-
-const cells = [];
-for (let i = 0; i < cellCountX; i++) {
-    cells[i] = [];
-    for (let j = 0; j < cellCountY; j++) {
-        // const c = new Cell(board, i, j);
-        // cells[i][j] = c;
-
-        if ((i + j) % 2 === 1) {
-            // c.on().draw();
-            const sprite = new PIXI.Sprite(onTexture);
-            sprite.position.x = i * Cell.width;
-            sprite.position.y = j * Cell.height;
-            stage.addChild(sprite);
-            sprite.onTexture = true;
-            cells[i][j] = sprite;
-        }
-        else {
-            // c.off().draw();
-            const sprite = new PIXI.Sprite(offTexture);
-            sprite.position.x = i * Cell.width;
-            sprite.position.y = j * Cell.height;
-            stage.addChild(sprite);
-            sprite.onTexture = false;
-            cells[i][j] = sprite;
-        }
-    }
-}
+Cell.initTextures(renderer);
+Cell.initCells(stage, cellCountX, cellCountY);
 
 requestAnimationFrame(animate);
 
 function animate() {
-    for (let i = 0; i < cellCountX; i++) {
-        for (let j = 0; j < cellCountY; j++) {
-            const sprite = cells[i][j];
-            sprite.onTexture = ! sprite.onTexture;
-            sprite.texture = sprite.onTexture ? onTexture : offTexture;
-            // cells[i][j].toggle().draw();
-        }
-    }
+    Cell.update();
 
-    board.render();
+    renderer.render(stage);
     requestAnimationFrame(animate);
 }
--- cell.js
+++ cell.js
@@ -1,46 +1,60 @@
 class Cell {
-    constructor(board, xIndex, yIndex) {
-        this.g = new PIXI.Graphics();
-        this.i = xIndex;
-        this.j = yIndex;
-
-        this.on();
-
-        board.add(this);
-    }
-
-    draw() {
-        this.g.clear();
-
-        const fill = this.active ? 0x000000 : 0xFFFFFF;
-        this.g.beginFill(fill);
-        // this.g.lineStyle(0, 0xFF0000, 1);
-
-        this.g.drawRect(this.i * Cell.width, this.j * Cell.height, Cell.width, Cell.height);
-        return this;
-    }
-
-    on() {
-        this.active = true;
-        return this;
-    }
-
-    off() {
-        this.active = false;
-        return this;
-    }
-
-    toggle() {
-        this.active = ! this.active;
-        return this;
-    }
-
     static size(width, height) {
         Cell.width = width;
         Cell.height = height;
     }
 
-    getGraphics() {
-        return this.g;
+    static initTextures(renderer) {
+        let g = new PIXI.Graphics();
+        g.beginFill(0x000000);
+        g.drawRect(0, 0, Cell.width, Cell.height);
+        Cell.onTexture = PIXI.RenderTexture.create(g.width, g.height);
+        renderer.render(g, Cell.onTexture);
+
+        g = new PIXI.Graphics();
+        g.beginFill(0xFFFFFF);
+        g.drawRect(0, 0, Cell.width, Cell.height);
+        Cell.offTexture = PIXI.RenderTexture.create(g.width, g.height);
+        renderer.render(g, Cell.offTexture);
+    }
+
+    static initCells(stage, cellCountX, cellCountY) {
+        Cell.cellsX = cellCountX;
+        Cell.cellsY = cellCountY;
+
+        for (let i = 0; i < Cell.cellsX; i++) {
+            Cell.cells[i] = [];
+            for (let j = 0; j < Cell.cellsY; j++) {
+
+                if ((i + j) % 2 === 1) {
+                    const sprite = new PIXI.Sprite(Cell.onTexture);
+                    sprite.position.x = i * Cell.width;
+                    sprite.position.y = j * Cell.height;
+                    stage.addChild(sprite);
+                    sprite.onTexture = true;
+                    Cell.cells[i][j] = sprite;
+                }
+                else {
+                    const sprite = new PIXI.Sprite(Cell.offTexture);
+                    sprite.position.x = i * Cell.width;
+                    sprite.position.y = j * Cell.height;
+                    stage.addChild(sprite);
+                    sprite.onTexture = false;
+                    Cell.cells[i][j] = sprite;
+                }
+            }
+        }
+    }
+
+    static update() {
+        for (let i = 0; i < Cell.cellsX; i++) {
+            for (let j = 0; j < Cell.cellsY; j++) {
+                const sprite = Cell.cells[i][j];
+                sprite.onTexture = ! sprite.onTexture;
+                sprite.texture = sprite.onTexture ? Cell.onTexture : Cell.offTexture;
+            }
+        }
     }
 }
+
+Cell.cells = [];
